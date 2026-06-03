68273338820cc95b884ab4b53c278133338e44d1
ArthurZambao <arthurzambaoo@gmail.com>
2026-06-03 16:15:06 -0300

    feat: add GymNotes project and constants configuration

--- src/shared/constants/projects.ts
+++ src/shared/constants/projects.ts
@@ -2,6 +2,5 @@ export const projects = [
   { title: 'D.A.F.E', img: '/dafe.png', link: 'https://dafe-project.vercel.app/', stack: ["next", "typescript", "tailwindcss", "nest", "mongodb"] },
   { title: 'Incita', img: '/incita.png', link: 'https://incita.online', stack: ["next", "typescript", "tailwindcss", "nest", "mongodb"] },
   { title: "Portfólio", img: '/portfolio.png', link: 'https://arthurzambao.online', stack: ["next", "typescript", "tailwindcss"] },
-  { title: 'Incita', img: '/incita.png', link: 'https://incita.online', stack: ["next", "typescript", "tailwindcss", "nest", "mongodb"] },
-
+  { title: "GymNotes", img: '/gymnotes.png', link: 'https://gymnotesapp.vercel.app/', stack: ["next", "typescript", "tailwindcss", "nest", "mongodb"] },
 ];
